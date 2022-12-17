bba779c33cee010da95b59e356c5bb52290f214d
Jeffrey Simon <jeffreywsimon6@yahoo.com>
2022-12-16 20:36:10 -0500

    12/16

---
+++ server/app.js
@@ -0,0 +1,55 @@
+// 'Import' the Express module instead of http
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose= require('mongoose');
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+// Initialize the Express application
+const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+   response.status(200).json({ message: "Service healthy" });
+});
+
+app.get("/users/:id", (request, response) => {
+  // express adds a "params" Object to requests
+  const id = request.params.id;
+  // handle GET request for post with an id of "id"
+  response.send(JSON.stringify({ user_id: id }));
+});
+
+
+app.post("/add", (request, response) => {
+  const num1 = request.body.numberOne;
+  const num2 = request.body.numberTwo;
+  const responseBody = {
+    sum: num1 + num2
+  };
+  response.json(responseBody);
+});
+
+
+// Tell the Express app to start listening
+// Let the humans know I am running and listening on 4040
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
